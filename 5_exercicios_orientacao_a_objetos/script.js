5b4086a718d87ab86caa2fceff85069a4ee23ca7
Ravi Silva <ravifelipee@gmail.com>
2024-09-20 14:18:32 -0300

    Solving the fifth question

--- 5_exercicios_orientacao_a_objetos/script.js
+++ 5_exercicios_orientacao_a_objetos/script.js
@@ -185,3 +185,115 @@ carroRavi.dirigirCarros(1);
 console.log(carroRavi.gasolinaRestante);
 carroRavi.aumetarGasolina(15);
 console.log(carroRavi.gasolinaRestante);
+
+//05
+class contaBancaria {
+    constructor(saldoContaCorrente, saldoContaPoupanca, jurosPoupanca) {
+        this.saldoContaCorrente = saldoContaCorrente;
+        this.saldoContaPoupanca = saldoContaPoupanca;
+        this.jurosPoupanca = jurosPoupanca;
+    }
+
+    depositoContaCorrente(valorDoDeposito) {
+        this.saldoContaCorrente = this.saldoContaCorrente + valorDoDeposito;
+        return this.saldoContaCorrente;
+    }
+
+    saqueContaCorrente(valorDoSaque) {
+        if (this.saldoContaCorrente > 0 && valorDoSaque <= this.saldoContaCorrente) {
+            this.saldoContaCorrente = this.saldoContaCorrente - valorDoSaque;
+            return this.saldoContaCorrente;
+
+        } else if (this.saldoContaCorrente <= 0) {
+            console.log(`Para fazer um saque você precisa ter um saldo superior a zero. Saldo Atual: ${this.saldoContaCorrente}`)
+
+        } else if (valorDoSaque > this.saldoContaCorrente) {
+            console.log(`Não é permitido realizar um saque maior do que o valor disponivel na sua conta. Você poderá realizar um saque de acordo com o seu saldo. Saldo Atual: ${this.saldoContaCorrente}`)
+        }
+    }
+
+    depositoContaPoupanca(valorDoDeposito) {
+        this.saldoContaPoupanca = this.saldoContaPoupanca + valorDoDeposito;
+        return this.saldoContaPoupanca
+    }
+
+    saqueContaPoupanca(valorDoSaque) {
+        if (this.saldoContaPoupanca > 0 && valorDoSaque <= this.saldoContaPoupanca) {
+            this.saldoContaPoupanca = this.saldoContaPoupanca - valorDoSaque;
+            return this.saldoContaPoupanca
+
+        } else if (this.saldoContaPoupanca <= 0) {
+            console.log(`Para fazer um saque você precisa ter um saldo superior a zero. Saldo Atual: ${this.saldoContaPoupanca}`)
+        } else if (valorDoSaque > this.saldoContaPoupanca) {
+            console.log(`Não é permitido realizar um saque maior do que o valor disponivel na sua conta. Você poderá realizar um saque de acordo com o seu saldo. Saldo Atual: ${this.saldoContaPoupanca}`)
+        }
+    }
+
+    transferenciaCorrenteParaPoupanca(valorTransferencia) {
+        if (valorTransferencia <= this.saldoContaCorrente && valorTransferencia > 0) {
+            this.saldoContaPoupanca = this.saldoContaPoupanca + valorTransferencia;
+            this.saldoContaCorrente = this.saldoContaCorrente - valorTransferencia;
+            return console.log(`Saldo Conta Poupança:${this.saldoContaPoupanca} - Saldo Conta Corrente: ${this.saldoContaCorrente}`);
+        } else if (valorTransferencia > this.saldoContaCorrente) {
+            console.log(`Esse valor não está disponivel para ser transferido da sua conta corrente. O seu saldo na conta corrente é R${this.saldoContaCorrente}`);
+        } else if (valorTransferencia < 0) {
+            console.log('Não é possivel realizar transferencias de valores negativos. Tente novamente')
+        }
+    }
+
+    transferenciaPoupancaParaCorrente(valorTransferencia) {
+        if (valorTransferencia <= this.saldoContaPoupanca && valorTransferencia > 0) {
+            this.saldoContaCorrente = this.saldoContaCorrente + valorTransferencia;
+            this.saldoContaPoupanca = this.saldoContaPoupanca - valorTransferencia;
+            return console.log(`Saldo Conta Poupança:${this.saldoContaPoupanca} - Saldo Conta Corrente: ${this.saldoContaCorrente}`);
+        } else if (valorTransferencia > this.saldoContaPoupanca) {
+            console.log(`Esse valor não está disponivel para ser transferido da sua conta poupança. O seu saldo na conta poupança é R${this.saldoContaPoupanca}`);
+        } else if (valorTransferencia < 0) {
+            console.log('Não é possivel realizar transferencias de valores negativos. Tente novamente')
+        }
+    }
+
+    jurosAniversario() {
+        let juros = (this.saldoContaPoupanca * this.jurosAniversario) / 100;
+        this.saldoContaPoupanca = this.saldoContaPoupanca + juros;
+    }
+
+}
+
+class contaEspecial extends contaBancaria {
+    constructor(saldoContaCorrente, saldoContaPoupanca, jurosPoupanca) {
+        super(saldoContaCorrente, saldoContaPoupanca, jurosPoupanca * 2)
+    }
+}
+
+
+let contaRavi02 = new contaBancaria(8000, 20000, 1);
+//Corente
+console.log(contaRavi02);
+contaRavi02.jurosAniversario;
+console.log(contaRavi02);
+
+console.log(contaRavi02.saldoContaCorrente)
+contaRavi02.depositoContaCorrente(20);
+console.log(contaRavi02.saldoContaCorrente);
+contaRavi02.saqueContaCorrente(30);
+console.log(contaRavi02.saldoContaCorrente);
+
+//Poupanca
+console.log(contaRavi02.saldoContaPoupanca)
+contaRavi02.depositoContaPoupanca(100);
+console.log(contaRavi02.saldoContaPoupanca);
+contaRavi02.saqueContaPoupanca(200);
+console.log(contaRavi02.saldoContaPoupanca);
+
+//Transferencias
+console.log(contaRavi02.saldoContaCorrente)
+console.log(contaRavi02.saldoContaPoupanca)
+contaRavi02.transferenciaCorrenteParaPoupanca(20);
+contaRavi02.transferenciaPoupancaParaCorrente(10);
+console.log(contaRavi02.saldoContaCorrente)
+console.log(contaRavi02.saldoContaPoupanca)
+
+let contaRavi03 = new contaEspecial(80, 700, 1);
+console.log(contaRavi03);
+contaRavi03.depositoContaPoupanca(12);
